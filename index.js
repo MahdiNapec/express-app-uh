8d5ecfa2532d01518c982d9c2c585986d9207910
MahdiNapec <mahdi.napec@gmail.com>
2026-04-19 13:58:49 +0100

    modifying the index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h3>docker container is running</h3>');
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Hello, Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 })
